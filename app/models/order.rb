6d83a1ac45f870c13db3fd55054480d6b3a5fee9
Paul <paulmaldonado73@gmail.com>
2017-12-28 21:40:34 -0600

    Se creo la validacion de los campos del formulario en el modelo order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+	validates :pedido. presence: true
+	validates :codigo, presence: true
+	validates :solicitud, presence: true
+	validates :surtido, presence: true
 end
